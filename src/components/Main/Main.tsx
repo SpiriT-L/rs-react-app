13102720e888485d92fcb6c1fdd05b9da7d0b6fa
LeonID <lmatskevich79@gmail.com>
2025-01-30 00:05:32 +0300

    feat: add component Main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <main>
+        <section>Search</section>
+        <section>Content</section>
+      </main>
+    );
+  }
+}
+
+export default Main;
