1cee8ebcef1052fb55f5674266b3df66dfa3946f
LeonID <lmatskevich79@gmail.com>
2025-02-08 13:48:59 +0300

    refactor: replaced the class component Home with a functional component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Main from '../../components/Main/Main';
 
-class Home extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
-    );
-  }
-}
+const Home: FC = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
