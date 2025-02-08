2b3d3d5922e1fd22cb78cbfa19ed35b96ae50ded
LeonID <lmatskevich79@gmail.com>
2025-02-08 13:31:20 +0300

    refactor: replaced the Footer class component with a functional component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,27 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import styles from './Footer.module.scss';
 
-class Footer extends Component {
-  state = {};
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <ul className={styles.list}>
-          <li className={styles.itemList}>
-            <a
-              className={styles.linkList}
-              href="https://rs.school/courses/reactjs"
-            >
-              RSSchool
-            </a>
-          </li>
-          <li className={styles.itemList}>React 2025 Q1</li>
-          <li className={styles.itemList}>
-            <a className={styles.linkList} href="https://github.com/SpiriT-L">
-              GitHub
-            </a>
-          </li>
-        </ul>
-      </footer>
-    );
-  }
-}
+const Footer: FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.list}>
+        <li className={styles.itemList}>
+          <a
+            className={styles.linkList}
+            href="https://rs.school/courses/reactjs"
+          >
+            RSSchool
+          </a>
+        </li>
+        <li className={styles.itemList}>React 2025 Q1</li>
+        <li className={styles.itemList}>
+          <a className={styles.linkList} href="https://github.com/SpiriT-L">
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
 export default Footer;
