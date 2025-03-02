b32ff7687fe398196e7f64dfa07f55ce2bca8b38
LeonID <lmatskevich79@gmail.com>
2025-03-02 20:50:26 +0300

    refactor: refactor tests Card.test.tsx

--- src/components/__tests__/Card.test.tsx
+++ src/components/__tests__/Card.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
